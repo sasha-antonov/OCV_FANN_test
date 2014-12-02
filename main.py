327a10d9f8a7bd113eb85461dec6a4a41f3caf13
sasha-antonov <sasha.antonov1989@gmail.com>
2014-12-02 20:23:17 +0400

    color object

--- main.py
+++ main.py
@@ -17,11 +17,46 @@ cv.CvtColor(img, gray, cv.CV_RGB2GRAY)
 cv.Not(gray, gray)
 cv.Threshold(gray, binary, 20, 255, cv.CV_THRESH_BINARY)
 
-"""" находим все белые связанные поля и
-     заливаем разными цветами
-"""""
+cv.iplimage.height
 
-cv.FloodFill(binary, (0,10), 100, cv.ScalarAll(10), cv.ScalarAll(10), 0, dst)
+color = 10
+color_list = []
+for x in range(binary.width):
+     for y in range(binary.height):
+          if(binary[y,x] > color):
+               cv.FloodFill(binary, (x,y), color, cv.ScalarAll(10), cv.ScalarAll(10), 0, dst)
+               color_list.append(color)
+               color = color + 10
+
+obj = []
+l_x = 0
+r_x = 0
+d_y = 0
+u_y = 0
+color = 0
+for col in range(len(color_list)):
+     for x in range(binary.width):
+          for y in range(binary.height):
+               if(binary[y,x] == color_list[col] and
+               binary[y,x] != color):
+                    obj.append([x,x,y,y])
+                    color = color_list[col]
+
+for n in range(len(color_list)):
+     for x in range(binary.width):
+          for y in range(binary.height):
+               if(binary[y,x] == color_list[n]):
+                    if(obj[n][0] > x):
+                         obj[n][0] = x
+                    if(obj[n][1] < x):
+                         obj[n][1] = x
+                    if(obj[n][2] > y):
+                         obj[n][2] = y
+                    if(obj[n][3] < y):
+                         obj[n][3] = y
+
+for l in range(len(obj)):
+     print(obj[l], color_list[l])
 
 crop_num_6 = binary[185:450, 90:250]
 crop_num_7 = binary[185:450, 370:530]
